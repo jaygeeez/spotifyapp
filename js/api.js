11db2476dca694f90d374f7008a18a28637bc8d2
jaygeeez <66766785+jaygeeez@users.noreply.github.com>
2024-07-25 18:02:27 -0400

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,6 @@
 // var redirect_uri = "https://makeratplay.github.io/SpotifyWebAPI/"; // change this your value
 var redirect_uri = "http://127.0.0.1:5500";
+//var redirect_uri = "https://jaygeeez.github.io/spotifyapp/index";
 
 var client_id = "b0967dcab86e44698289a22765cf49dc";
 var client_secret = "14aa4e5fce0443a4a4bca54c5ba51ba2"; // In a real app you should not expose your client_secret to the user
@@ -56,10 +57,8 @@ function getCode() {
 }
 
 function requestAuthorization() {
-  // client_id = document.getElementById("clientId").value;
-  // client_secret = document.getElementById("clientSecret").value;
-  localStorage.setItem("client_id", client_id);
-  localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
+  //localStorage.setItem("client_id", client_id);
+  //localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
 
   let url = AUTHORIZE;
   url += "?client_id=" + client_id;
@@ -153,12 +152,33 @@ function shuffle(array) {
 }
 
 var correct_artist;
+var correct_album;
+var correct_track;
+var album_Track;
 var score = 0;
 
 function callQuestion(id = "option_1") {
-  if (document.getElementById(id).innerHTML == correct_artist) {
+  if (
+    document.getElementById(id).innerHTML == correct_artist ||
+    document.getElementById(id).innerHTML == correct_album ||
+    document.getElementById(id).innerHTML == correct_track
+  ) {
     score += 1;
   }
+
+  // Determine which of the 3 questions are chosen
+  let randomQuestion = getRandomNum(1, 4);
+  console.log(randomQuestion);
+  if (randomQuestion == 1) {
+    whichArtist();
+  } else if (randomQuestion == 2) {
+    whichAlbum();
+  } else if (randomQuestion == 3) {
+    whichTopTrack();
+  }
+}
+
+function whichArtist() {
   fetch("https://api.spotify.com/v1/me/tracks?market=CA&limit=50", {
     method: "GET",
     headers: {
@@ -200,7 +220,112 @@ function callQuestion(id = "option_1") {
     });
 }
 
+function whichAlbum() {
+  fetch("https://api.spotify.com/v1/me/albums?limit=50", {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + access_token,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      // Random Number Thing
+      let correct_number = getRandomNum(0, 49);
+      correct_album = getAlbum(response, correct_number);
+      let albums = [];
+      albums.push(correct_album);
+
+      // Repeat until there are four total answers
+      while (albums.length < 4) {
+        let wrong_number = getRandomNum(0, 49);
+
+        // Doesn't let the right answer appear twice
+        if (!albums.includes(getAlbum(response, wrong_number))) {
+          albums.push(getAlbum(response, wrong_number));
+        }
+      }
+
+      shuffle(albums);
+
+      let song_title = JSON.stringify(getAlbumTrack(response, correct_number));
+
+      document.getElementById("score").innerHTML = "Score: " + score;
+
+      // What is the album of the song
+      document.getElementById("song_text").innerHTML =
+        "Which album is: " + song_title + " from?";
+      document.getElementById("option_1").innerHTML = albums[0];
+      document.getElementById("option_2").innerHTML = albums[1];
+      document.getElementById("option_3").innerHTML = albums[2];
+      document.getElementById("option_4").innerHTML = albums[3];
+    });
+}
+
+function whichTopTrack() {
+  fetch("https://api.spotify.com/v1/me/tracks?market=CA&limit=50", {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + access_token,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      // get artists from profile tracks, use artists id to find their top tracks
+
+      // Random Number Thing
+      let random_number = getRandomNum(0, 49);
+      random_artist = response.items[random_number].track.artists[0].name;
+      random_id = response.items[random_number].track.artists[0].id;
+      let topTracks = [];
+
+      let tracks_url =
+        "https://api.spotify.com/v1/artists/" +
+        random_id +
+        "/top-tracks?market=CA";
+
+      fetch(tracks_url, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + access_token,
+        },
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          for (var i = 0; i < 4; i++) {
+            topTracks.push(response.tracks[i].name);
+          }
+
+          correct_artist = topTracks[0];
+          shuffle(topTracks);
+
+          document.getElementById("score").innerHTML = "Score: " + score;
+
+          // Who is the artist of song
+          document.getElementById("song_text").innerHTML =
+            "What is: " + random_artist + "'s top track?";
+          document.getElementById("option_1").innerHTML = topTracks[0];
+          document.getElementById("option_2").innerHTML = topTracks[1];
+          document.getElementById("option_3").innerHTML = topTracks[2];
+          document.getElementById("option_4").innerHTML = topTracks[3];
+        });
+    });
+}
+
 function getArtist(response, number) {
   let artist_name = response.items[number].track.artists[0].name;
   return artist_name;
+}
+
+function getAlbum(response, number) {
+  let albumName = response.items[number].album.name;
+  return albumName;
+}
+
+function getAlbumTrack(response, albumNum) {
+  let randomlength = response.items[albumNum].album.tracks.items.length;
+  let albumTrack =
+    response.items[albumNum].album.tracks.items[
+      getRandomNum(0, randomlength - 1)
+    ].name;
+  return albumTrack;
 }
